98cb6ace3dc46f6dc90343f93eb43ec5630f8945
José Pedro Dias <jose.pedro.dias@gmail.com>
2014-07-29 03:17:12 +0100

    refactoring done. could not test yet though

---
+++ sample.js
@@ -0,0 +1,58 @@
+var vncbot = require('./vncbot');
+
+var cfg = require('./cfg.json');
+
+
+
+var clone = function(o) {
+    return JSON.parse( JSON.stringify(o) );
+};
+
+
+
+var v = vncbot(cfg);
+
+
+
+setTimeout(function() {
+
+    if (1) {
+        var stuff = 'echo "hello world"'.split('');
+        stuff.push('enter');
+        //v.sendKeysNow(stuff);
+        v.sendKeys(stuff, 100);
+    }
+
+
+
+    if (0) {
+        var points = [];
+        (function() {
+            var I = 32;
+            var r = 100;
+            var a;
+            var da = Math.PI * 2 / I;
+            var ctr = [300, 250];
+            for (var i = 0; i < I; ++i) {
+                a = i * da;
+                points.push([
+                    Math.round( ctr[0] + r * Math.cos(a) ),
+                    Math.round( ctr[1] + r * Math.sin(a) ),
+                    1
+                ]);
+            }
+        })();
+        var p = clone(points[0]);
+        p[2] = 0;
+        points.unshift(p);
+        points.push(p);
+        v.sendPointers(points, 25);
+    }
+
+
+
+    //v.update(0, 0, 128, 128);
+
+    v.redraw();
+
+}, 2000);
--- vncbot.js
+++ vncbot.js
@@ -4,7 +4,49 @@ var assert = require('assert');
 var RFB    = require('rfb'); // https://github.com/substack/node-rfb/blob/master/index.js
 var PNG    = require('png');
 
-var keys   = require('./keys');
+
+
+var keys = {
+    backspace     : 0xff08,
+    bs            : 0xff08,
+    tab           : 0xff09,
+    'return'      : 0xff0d,
+    enter         : 0xff0d,
+    escape        : 0xff1b,
+    insert        : 0xff63,
+    'delete'      : 0xffff,
+    del           : 0xffff,
+    home          : 0xff50,
+    end           : 0xff57,
+    page_up       : 0xff55,
+    page_down     : 0xff56,
+    left          : 0xff51,
+    up            : 0xff52,
+    right         : 0xff53,
+    down          : 0xff54,
+    f1            : 0xffbe,
+    f2            : 0xffbf,
+    f3            : 0xffc0,
+    f4            : 0xffc1,
+    f5            : 0xffc2,
+    f6            : 0xffc3,
+    f7            : 0xffc4,
+    f8            : 0xffc5,
+    f9            : 0xffc6,
+    f10           : 0xffc7,
+    f11           : 0xffc8,
+    f12           : 0xffc9,
+    shift_left    : 0xffe1,
+    shift_right   : 0xffe2,
+    control_left  : 0xffe3,
+    control_right : 0xffe4,
+    meta_left     : 0xffe7,
+    meta_right    : 0xffe8,
+    alt           : 0xffe9,
+    alt_left      : 0xffe9,
+    alt_right     : 0xffea,
+    alt_gr        : 0xffea
+};
 
 
 
@@ -22,9 +64,58 @@ var keyToHex = function(s) {
 
 
 
+var setRectAlpha = function(rect) {
+    var f = rect.fb;
+    var I = rect.width * rect.height;
+    var j, t;
+    for (var i = 0; i < I; ++i) {
+        j = i * 4;
+        f[j + 3] = 0;
+    }
+};
+
+
+
+var saveRectNow = function(rect, path) {
+    var png = new PNG.Png(rect.fb, rect.width, rect.height, 'bgra'); // rgba bgra
+    setRectAlpha(rect);
+    var data = png.encodeSync();
+    fs.writeFileSync(path, data, 'binary');
+    console.log('saved ' + rect.width + 'x' + rect.height + ' to ' + path);
+};
+
+
+
+var saveRect = function(rect, path, cb) {
+    var png = new PNG.Png(rect.fb, rect.width, rect.height, 'bgra'); // rgba bgra
+    setRectAlpha(rect);
+    png.encode(function(data) {
+        fs.writeFile(path, data, 'binary', function(err) {
+            cb(err);
+            if (!err) {
+                console.log('saved ' + rect.width + 'x' + rect.height + ' to ' + path);
+            }
+        });
+    });
+};
+
+
+
+var DEBUG = true;
+
+
+
 var vncbot = function(cfg) {
 
     var r = new RFB(cfg);
+
+
+
+    r.on('raw', function(rect) {
+        //console.log('-> RAW');
+        assert.equal(rect.bitsPerPixel, 32);
+        saveRectNow(rect, '/tmp/x.png');
+    });
 
     r.on('error', function(err) {
         console.error('ERROR', err);
@@ -37,6 +128,75 @@ var vncbot = function(cfg) {
     
 
     var api = {
+
+        // KEY INPUT
+
+        sendKeysNow: function(queue) {
+            queue.forEach(function(k) {
+                var kk = keyToHex(k);
+                r.sendKey(kk, 1);
+                r.sendKey(kk, 0);
+            });
+        },
+
+        sendKeys: function(queue, dt) {
+            var timer;
+
+            var onTimer = function() {
+                var k = queue.shift();
+                if (k === undefined) {
+                    return clearInterval(timer);
+                }
+                if (DEBUG) { console.log('KEY: ' + k); }
+                k = keyToHex(k);
+                r.sendKey(k, 1);
+                r.sendKey(k, 0);
+            };
+
+            timer = setInterval(onTimer, dt);
+        },
+
+        sendKey: function(k, isDown) {
+            if (typeof k === 'string') {
+                k = keyToHex(k);
+            }
+            r.sendKey(k, isDown);
+        },
+
+
+
+        // POINTER INPUT
+
+        sendPointers: function(queue, dt) {
+            var timer;
+
+            var onTimer = function() {
+                var p = queue.shift();
+                if (p === undefined) {
+                    return clearInterval(timer);
+                }
+                if (DEBUG) { console.log('POINTER: ' + p); }
+                r.sendPointer(p[0], p[1], p[2]); // x, y, mask
+            };
+
+            timer = setInterval(onTimer, dt);
+        },
+
+        sendPointer: function(x, y, mask) {
+            r.sendPointer(x, y, mask);
+        },
+
+
+
+        // SCREEN OUTPUT
+
+        redraw: function() {
+            r.requestRedraw();
+        },
+
+        update: function(x, y, w, h) {
+            r.requestUpdate({x:x, y:y, width:w, height:h});
+        }
 
     };
 
